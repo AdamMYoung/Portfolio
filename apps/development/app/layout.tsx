745e4d44864db051de19497d87425b495c94e56f
Adam Young <adam.young@checkout.com>
2023-04-18 12:01:14 +0100

    Initial homepage changes 

--- apps/development/app/layout.tsx
+++ apps/development/app/layout.tsx
@@ -1,37 +1,9 @@
 /* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import React from "react";
-import { Roboto_Mono } from "next/font/google";
-import { FiMenu, FiGithub, FiTwitter } from "react-icons/fi";
 import Script from "next/script";
 
 import "../styles/globals.css";
-
-import {
-  LayoutDesktop,
-  LayoutDesktopBody,
-  LayoutDesktopNavigation,
-  LayoutDesktopSidebar,
-  LayoutDesktopHeading,
-  LayoutDesktopSubheading,
-  LayoutDesktopTitle,
-  Link,
-  IconList,
-  LayoutMobile,
-  LayoutMobileBody,
-  LayoutMobileDrawer,
-  LayoutMobileDrawerButton,
-  LayoutMobileDrawerItem,
-  LayoutMobileNavigation,
-  LayoutMobileTitle,
-  LayoutMobileHeading,
-  LayoutMobileSubheading,
-} from "./client-components";
-
-const robotoMono = Roboto_Mono({
-  weight: ["300", "400", "700"],
-  subsets: ["latin"],
-  variable: "--font-roboto-mono",
-});
+import Link from "next/link";
 
 type PageLayoutProps = {
   children: React.ReactNode;
@@ -40,81 +12,17 @@ type PageLayoutProps = {
 export default async function PageLayout({ children }: PageLayoutProps) {
   return (
     <html lang="en-gb">
-      <body className={`${robotoMono.variable} bg-gray-900 font-sans  text-white`}>
-        <LayoutDesktop>
-          <LayoutDesktopSidebar>
-            <Link href="/">
-              <LayoutDesktopTitle role="button">
-                <LayoutDesktopHeading>Adam Young</LayoutDesktopHeading>
-                <LayoutDesktopSubheading>Development</LayoutDesktopSubheading>
-              </LayoutDesktopTitle>
-            </Link>
-            <LayoutDesktopNavigation>
-              {/* <Link className="hover:underline" href="/history">
-                History
-              </Link> */}
-              <Link href="/projects">Projects</Link>
-              <Link href="/contact">Contact</Link>
-              <IconList>
-                <Link type="external" href="https://github.com/AdamMYoung">
-                  <FiGithub />
-                </Link>
-                <Link type="external" href="https://twitter.com/AdamMYoung_">
-                  <FiTwitter />
-                </Link>
-              </IconList>
-            </LayoutDesktopNavigation>
-          </LayoutDesktopSidebar>
-          <LayoutDesktopBody className="mx-auto mb-4 flex w-full pr-8">
-            <div className="mx-auto max-w-4xl md:pt-12">{children}</div>
-          </LayoutDesktopBody>
-        </LayoutDesktop>
-        <LayoutMobile>
-          <LayoutMobileNavigation className="bg-gray-900">
-            <Link href="/">
-              <LayoutMobileTitle role="button">
-                <LayoutMobileHeading>Adam Young</LayoutMobileHeading>
-                <LayoutMobileSubheading>Development</LayoutMobileSubheading>
-              </LayoutMobileTitle>
-            </Link>
-            <LayoutMobileDrawerButton aria-label="Open menu">
-              <FiMenu className="h-8 w-8" />
-            </LayoutMobileDrawerButton>
-            <LayoutMobileDrawer className=" bg-gray-900">
-              <LayoutMobileDrawerItem className="py-2 hover:bg-gray-700" href="/">
-                Home
-              </LayoutMobileDrawerItem>
-              {/* <LayoutMobileDrawerItem className="py-2 px-4 hover:bg-gray-700" href="/history">
-                History
-              </LayoutMobileDrawerItem> */}
-              <LayoutMobileDrawerItem className="py-2 hover:bg-gray-700" href="/projects">
-                Projects
-              </LayoutMobileDrawerItem>
-              <LayoutMobileDrawerItem className="py-2 hover:bg-gray-700" href="/contact">
-                Contact
-              </LayoutMobileDrawerItem>
-              <LayoutMobileDrawerItem
-                className="flex items-center gap-2 py-2 hover:bg-gray-700"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="GitHub"
-                href="https://github.com/AdamMYoung"
-              >
-                <FiGithub /> GitHub
-              </LayoutMobileDrawerItem>
-              <LayoutMobileDrawerItem
-                className="flex items-center gap-2 py-2 hover:bg-gray-700"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Twitter"
-                href="https://twitter.com/AdamMYoung_"
-              >
-                <FiTwitter /> Twitter
-              </LayoutMobileDrawerItem>
-            </LayoutMobileDrawer>
-          </LayoutMobileNavigation>
-          <LayoutMobileBody className="p-8">{children}</LayoutMobileBody>
-        </LayoutMobile>
+      <body className="py-16 flex flex-col gap-12">
+        <nav className="grid grid-cols-[1.5fr_1fr] gap-4 justify-between text-4xl section-container">
+          <div>Some Logo</div>
+
+          <div className="flex gap-6">
+            <Link href="/">About</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/contact">Contact</Link>
+          </div>
+        </nav>
+        <main>{children}</main>
       </body>
       {process.env.NODE_ENV === "production" && (
         <>
--- apps/development/app/page.tsx
+++ apps/development/app/page.tsx
@@ -5,38 +5,47 @@ import selfie from "../src/images/selfie-snow.jpg";
 export default function Home() {
   return (
     <div className="flex flex-col gap-8">
-      <div className="grid gap-8 md:grid-cols-2">
-        <div className="flex flex-col gap-8">
-          <h1 className="text-4xl font-bold">Hi, I&apos;m Adam Young!</h1>
+      {/* Hero Card */}
+      <div className="grid grid-cols-[1.5fr_1fr] pb-16 section-container">
+        <div className="flex flex-col gap-24">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-8xl tracking-wide font-medium">
+              Adam <br /> Young.
+            </h1>
+            <div className="border-b-8 w-24" />
+          </div>
+          <div className="flex gap-4">
+            <a>Twitter</a>
+            <a>GitHub</a>
+            <a>LinkedIn</a>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h2 className="tracking-wider">Introduction</h2>
+          <p className="text-3xl">Front-end Software Engineer, based in Darlington, UK</p>
           <p>
-            I&apos;m a Software Engineer currently working at Checkout.com. My focus is currently on React and Next.js,
-            as well as any new and interesting libraries that come my way.
-          </p>
-          <p>
-            When I&apos;m not working, you can probably find me hiking, flying drones, or taking pictures, more of which
-            you can find on my{" "}
-            <a className="underline" target="_blank" rel="noreferrer" href="https://photography.aydev.uk">
-              photography site
-            </a>
-            .
-          </p>
-
-          <p className="hidden md:block">
-            Use the navigation bar to the left to have a look around, and let me know what you think!
-          </p>
-          <p className="block md:hidden">
-            Use the navigation menu at the top to have a look around, and let me know what you think!
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
           </p>
         </div>
-        <Image priority alt="Selfie in the snow" src={selfie} className="rounded" />
       </div>
 
-      <p></p>
+      <div className="flex py-16 bg-gray-100">
+        {/* Logos */}
+        <div className="flex gap-6 mx-auto">
+          <p>Logo 1</p>
+          <p>Logo 2</p>
+          <p>Logo 3</p>
+        </div>
+      </div>
     </div>
   );
 }
 
 export const metadata: Metadata = {
-  title: "Adam Young | Development",
-  description: "Development portfolio of Software Engineer, Adam Young",
+  title: "Home | AYDev",
+  description: "Hi, I'm Adam Young. I'm a software developer, photographer and avid hiker. Why not have a look around?",
 };

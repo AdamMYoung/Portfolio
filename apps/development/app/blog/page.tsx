34b3d6e1c8da0b36df0e5c3469e5ea2f43a7d9a6
Adam Young <adam.young@checkout.com>
2023-04-18 12:01:45 +0100

    app layout structure tweaks 

---
+++ apps/development/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import Image from "next/image";
+
+export default function Blog() {
+  return <div />;
+}
+
+export const metadata: Metadata = {
+  title: "Adam Young | Blog",
+  description: "Learn about software development, the web, or just what's going on in my life at the moment.",
+};

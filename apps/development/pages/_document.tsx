4592ec114713363b21ff112cd26975a629838dac
Adam Young <adamyoungg97@gmail.com>
2023-07-19 22:06:41 +0100

    Next bump and added metadata

--- apps/development/pages/_document.tsx
+++ apps/development/pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {process.env.NODE_ENV === "production" && (
+          <>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-WGNHV27GTK" strategy="afterInteractive" />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WGNHV27GTK');
+        `}
+            </Script>
+          </>
+        )}
+      </Head>
       <body>
         <Main />
         <NextScript />

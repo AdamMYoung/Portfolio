c2a7aa7a3b6d31f11ace0bdee3e6049ee36f15db
Adam Young <adamyoungg97@gmail.com>
2023-11-12 22:42:24 +0000

    Update GridImage.tsx

--- apps/photography/src/components/grid-image/GridImage.tsx
+++ apps/photography/src/components/grid-image/GridImage.tsx
@@ -10,13 +10,14 @@ type GridImageProps = {
   isPriority?: boolean;
 };
 
-export const GridImage = ({ image, placeholder, isPriority }: GridImageProps) => {
+export const GridImage = async ({ image, placeholder, isPriority }: GridImageProps) => {
   return (
     <div className="relative w-full" style={{ aspectRatio: image.exif.width / image.exif.height }}>
       <NextImage
         priority={isPriority}
         alt=""
-        placeholder={placeholder}
+        placeholder="blur"
+        blurDataURL={placeholder}
         src={image.path}
         fill
         sizes="(max-width: 1024px) 50vw, 25vw"

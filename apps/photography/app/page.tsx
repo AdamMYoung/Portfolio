2f50e42993efd6a28e885f88c7c6f01e6732d0ea
Adam Young <adamyoungg97@gmail.com>
2023-11-12 22:26:01 +0000

    Fix to types

--- apps/photography/app/page.tsx
+++ apps/photography/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
     <ImageGrid className="md:mr-2">
       {images.map((image, index) => (
         <div key={image.path} className="h-fit w-full">
-          <GridImage index={index} image={image} isPriority={index <= 30} />
+          <GridImage index={index} image={image} placeholder={image.placeholder} isPriority={index <= 30} />
         </div>
       ))}
     </ImageGrid>
